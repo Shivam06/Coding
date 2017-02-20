c206ba953c486eb3f8ca5dc8609cbbd8c39afa9b
Shivam Mahajan <shivammahajan6596@gmail.com>
2017-02-20 08:31:35 +0530

    Fixed some bugs

--- Graph/bfs_list_undirected.cpp
+++ Graph/bfs_list_undirected.cpp
@@ -1,16 +1,27 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<algorithm>
 using namespace std;
+
+bool find(vector<int> v, int a) {
+	for (int i=0; i < v.size(); i++) {
+		if(v[i] ==a)
+			return true;
+	}
+	return false;
+}
 
 void construct_adj_list(vector<int> adj[], int nodes, int edges, bool isdirected = false) {
 	int x,y;
 	cout << "Input start and end node" << endl;
 	for(int i=0; i<edges; i++) {
 		cin>>x>>y;
-		if(!isdirected)
+		if(!isdirected && !find(adj[y], x))
 			adj[y].push_back(x);
-		adj[x].push_back(y);
+		if (!find(adj[x],y))
+			adj[x].push_back(y);
+		
 	}  // How to return vector array??
 }
 
@@ -28,9 +39,11 @@ void display_graph(vector<int> adj[], int nodes) {
 
 void bfs(vector<int> adj[], int nodes, int start_node) {
 	queue<int> nodeQ;
-	int track_nodes[6] = {0};
+	int track_nodes[nodes + 1] = {0};
 	track_nodes[0] = 1;
-	cout << start_node << " ";
+	
+	int level = 0;
+	cout << start_node<<" ";
 	track_nodes[start_node] = 1;
 	nodeQ.push(start_node);
 	while(!nodeQ.empty()) {
@@ -38,10 +51,11 @@ void bfs(vector<int> adj[], int nodes, int start_node) {
 		nodeQ.pop();
 		if (!adj[s].size())
 			continue;
+		level++;
 		for (int i = 0; i < adj[s].size(); i++){
 			if (!track_nodes[adj[s][i]]) {
 				track_nodes[adj[s][i]] = 1;
-				cout << adj[s][i] << " ";
+				cout << adj[s][i] <<" ";
 				nodeQ.push(adj[s][i]);
 			}
 			else
@@ -55,7 +69,7 @@ int main() {
 	cout <<"Input number of nodes and edges"<<endl;
 	cin>>nodes>>edges;
 	vector<int> adj[nodes+1];
-	construct_adj_list(adj, nodes, edges, true);
+	construct_adj_list(adj, nodes, edges);
 	display_graph(adj, nodes);
 	cout << endl;
 	bfs(adj, nodes, 1);
