8119b3766e4a706e472784255b3ac14eda02e359
Shivam Mahajan <shivammahajan6596@gmail.com>
2017-02-24 11:54:31 +0530

    Coded weighted graph implementation

--- Graph/weighted_undirected_graph_implementation.cpp
+++ Graph/weighted_undirected_graph_implementation.cpp
@@ -7,19 +7,24 @@ using namespace std;
 int main() {
 	int nodes, edges;
 	cin>>nodes>>edges;
-	vector<pair<int, int>> G[nodes];
+	vector<pair<int, int>> G[nodes+1];
 	int x,y,w;
 	for (int i = 0; i<edges; i++) {
 		cin>>x>>y>>w;
 		G[x].push_back({y,w});
+		G[y].push_back({x,w});
 	}
 
-	for (int i = 0; i < nodes; i++) {
-		cout << i << "-->";
-		for (int j = 0; j<G[i].size()-1; j++) {
-			cout << G[i][j].first << "("<<G[i][j].second<<")-->";
+	for (int i = 1; i <= nodes; i++) {
+		cout << i << "--";
+		int j = 0;
+		if (G[i].size() == 0)
+			continue;
+
+		for (; j<G[i].size()-1; j++) {
+			cout << "(" <<G[i][j].second <<")"<< "-->"<<G[i][j].first<<"--";
 		}
-		cout << G[i][j].first << "("<<G[i][j].second<<")"<<endl;
+		cout << "(" << G[i][j].second << ")" << "-->" << G[i][j].first<<endl;
 	}
 	return 0;
 }
