ec8980921a8fb18ad51af2cedb1c219438356811
unknown <shivammahajan6596@gmail.com>
2017-07-24 19:39:07 +0530

    Basic Solution to knight's distance problem

--- Backtracking/knight.cpp
+++ Backtracking/knight.cpp
@@ -1,42 +1,65 @@
 #include<iostream>
 using namespace std;
+#define N 4
 
 int x_moves[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
 int y_moves[8] = {-1, 1, -2, 2, -2, 2, 1, -1};
 
-int isSafe(int row, int col, int sol[8][8]) {
-	if ((row < 8 || row >=0) && (col < 8 || col >= 0) && (sol[row][col] == 0))  
+int isSafe(int row, int col, int chess[][N]) {
+	if ((row < N && row >=0) && (col < N && col >= 0) && (chess[row][col] == -1))  
 		return 1;
 	else
 		return 0;
 }
 
-bool knight(int sol[][8], int row, int col, int count) {
-	if (count == 64)
+bool knight_helper(int chess[N][N], int x, int y, int count) {
+	if (count == N*N + 1) {
 		return true;
-	sol[row][col] = count;
-
-	for (int i = 0; i<8; i++) {
-		int new_x = row + x_moves[i];
-		int new_y = col + y_moves[i];
-
-		if (isSafe(new_x, new_y, sol)) {
-			return knight(sol, new_x, new_y, count + 1);
-		}
 	}
+
+
+	for (int i = 0; i < 8; i++) {
+		int new_x = x + x_moves[i];
+		int new_y = y + y_moves[i];
+
+		cout << new_x << " "<<new_y << endl;
+		if (isSafe(new_x, new_y, chess)) {
+			chess[new_x][new_y] = count;
+			if (knight_helper(chess, new_x, new_y, count + 1))
+				return true;
+			chess[new_x][new_y] = -1;	
+		}
+		else
+			continue;
+	}
+	chess[x][y] = 0;
 	return false;
 }
 
-int main() {
-	int sol[8][8] = {0};
-	int row = 0, col = 0, count = 1;
-	knight(sol, row, col, count);
+bool knight(int chess[N][N]) {
+	int count = 1;
+	int x = 0, y = 0;
+	chess[x][y] = 0;
+	return knight_helper(chess, x, y, count);
+}
 
-	for (int i = 0; i<8; i++) {
-		for (int j = 0; j<8; j++) {
-			cout << sol[i][j] << " ";
+int main() {
+
+	int chess[N][N];
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++){
+			chess[i][j] = -1;
+		}
+	}
+
+	cout << knight(chess) << endl;
+
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++) {
+			cout << chess[i][j] << "    ";
 		}
 		cout << endl;
 	}
+
 	return 0;
 }
